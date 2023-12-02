aed37b55ebbb04d80f47bca01b15262f5ebb14c4
AxuAnnn <141130721+AxuAnnn@users.noreply.github.com>
2023-12-02 20:18:17 +0800

    Update database.py

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgres://default:dU7CNP4DVsyX@ep-crimson-brook-99002680.us-east-1.postgres.vercel-storage.com:5432/verceldb"
+SQLALCHEMY_DATABASE_URL = "postgresql://default:dU7CNP4DVsyX@ep-crimson-brook-99002680.us-east-1.postgres.vercel-storage.com:5432/verceldb"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
